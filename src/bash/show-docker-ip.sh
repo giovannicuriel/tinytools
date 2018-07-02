9c6e94db3cc9bbc0e5c6115b209ca7e7cf4d50f7
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-07-02 16:34:44 -0300

    Changing output format for show-docker-ip script

--- src/bash/show-docker-ip.sh
+++ src/bash/show-docker-ip.sh
@@ -6,5 +6,6 @@ docker_containers=$(docker ps  --format '{{.Names}}')
 echo 
 for i in ${docker_containers}; do
     ip=$(docker inspect ${i} --format "{{.NetworkSettings.Networks.${default_network}.IPAddress}}")
-    echo "${i}: ${ip}"
+    exported=$(echo "${i}: ${ip}" | sed -E 's/.*_(.*)_[[:digit:]]: *(.*)/\1_HOST=\"\2\"/g' | sed -E 's/-/_/g' | sed  -E 's/(.*)/export \U\1/')
+    echo ${exported}
 done;
