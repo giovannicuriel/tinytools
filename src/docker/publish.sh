5c16da0c5d76e24b163f4e761ce0ea3f0a2d7b3d
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-09-11 00:00:18 -0300

    adding print

--- src/docker/publish.sh
+++ src/docker/publish.sh
@@ -4,6 +4,12 @@ echo "Checking travis variables"
 echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 
+if [ ${TRAVIS_PULL_REQUEST} == "true" ]; then
+echo "This is a PR"
+else
+echo "This is not a PR"
+fi
+
 version="latest"
 if [ $TRAVIS_BRANCH != "master" ] ; then
   version=$TRAVIS_BRANCH
