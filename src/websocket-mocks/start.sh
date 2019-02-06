a860198ba69fbdb6f4e74491668b0a4f42004145
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-06 14:56:49 -0200

    start npm in background

--- src/websocket-mocks/start.sh
+++ src/websocket-mocks/start.sh
@@ -4,5 +4,5 @@ ADMIN_TOKEN=$(curl -H "Content-Type: application/json; charset=utf-8" "http://${
 print(json.load(sys.stdin)["jwt"])')
 SOCKET_TOKEN=$(curl -H "Content-Type: application/json; charset=utf-8" -sH "Authorization: Bearer $ADMIN_TOKEN" "http://${HOST}:8000/stream/socketio" | python3 -c 'import sys, json
 print(json.load(sys.stdin)["token"])')
-PORT=3002
-npm start -- -s "http://${HOST}:8000/" -c socketio -t $SOCKET_TOKEN
+nohup npm start -- -s "http://${HOST}:8000/" -c socketio -t $SOCKET_TOKEN &
+sleep 1
