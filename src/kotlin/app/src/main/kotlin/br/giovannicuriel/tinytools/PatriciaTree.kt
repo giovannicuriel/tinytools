8242de4efd5b59e8c2f5807ceee8a75a57d3eafe
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2021-11-06 19:07:22 -0300

    fixing patricia tree

--- src/kotlin/app/src/main/kotlin/br/giovannicuriel/tinytools/PatriciaTree.kt
+++ src/kotlin/app/src/main/kotlin/br/giovannicuriel/tinytools/PatriciaTree.kt
@@ -2,13 +2,12 @@ package br.giovannicuriel.tinytools
 
 
 class PatriciaTree {
-
     val rootNode = Node("^", 0.0, mutableMapOf(), 0)
 
     private fun findLongestMatchingIndex(s1: String, s2: String): Int = s1.commonPrefixWith(s2).length
 
     private fun findInChildren(currNode: Node, subKey: String, relaxed: Boolean) =
-        currNode.children.get(subKey[0])?.let {
+        currNode.children[subKey[0]]?.let {
             findNode(subKey, currNode, relaxed)
         }
 
@@ -16,11 +15,7 @@ class PatriciaTree {
         return findLongestMatchingIndex(subKey, currNode.key).let { matchingIndex ->
             when {
                 // Could be currNode itself or one of its children
-                matchingIndex == currNode.key.length -> findInChildren(
-                    currNode,
-                    subKey.substring(matchingIndex),
-                    relaxed
-                ) ?: currNode
+                matchingIndex == currNode.key.length -> findInChildren(currNode, subKey.substring(matchingIndex), relaxed) ?: currNode
                 // Incomplete key - no such key was found
                 matchingIndex > 0 -> if (relaxed) currNode else null
                 else -> null
@@ -29,21 +24,15 @@ class PatriciaTree {
     }
 
     fun findNode(key: String, relaxed: Boolean = false) =
-        rootNode.children.get(key[0])?.let { findNode(key, it, relaxed) }
+        rootNode.children[key[0]]?.let { findNode(key, it, relaxed) }
 
     private fun splitNode(node: Node, newIndex: Int) = node.apply {
         val nodeChildren = mutableMapOf<Char, Node>().apply { putAll(children) }
-        val prefixKey = node.key.substring(newIndex, node.key.length)
+        val prefixKey = key.substring(newIndex, key.length)
         children.clear()
         children.putAll(
             mutableMapOf(
-                prefixKey[0] to
-                        Node(
-                            prefixKey,
-                            node.data,
-                            nodeChildren,
-                            index + newIndex
-                        )
+                prefixKey[0] to Node(prefixKey, node.data, nodeChildren, index + newIndex)
             )
         )
         key = key.substring(0, newIndex)
